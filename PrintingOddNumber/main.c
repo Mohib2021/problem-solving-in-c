a85de6c8b27dd6f03426a5bd156ea05a9655be88
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-03-05 02:26:46 +0600

    Printing odd number

---
+++ PrintingOddNumber/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // Printing odd number from 5 to 35
+    for (int i = 5; i <= 35; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d \n", i);
+        }
+        }
+
+    return 0;
+}
