6177d055b6e3791eb91015fe7f0f308cd8ce06d6
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-03-05 00:51:47 +0600

    circle radius to area solved

---
+++ CircleRadiusToArea/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // circle radius to area calculation
+    int radius = 2;
+    float PI = 3.142857;
+    float area = PI * radius * radius;
+    printf("The area of circle is %.2f", area);
+
+    return 0;
+}
