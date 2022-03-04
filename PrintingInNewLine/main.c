617c9817b0d24b6c11dab15c3dd3c439919c4fcf
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-03-05 02:03:39 +0600

    printing info in new line

---
+++ PrintingInNewLine/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // Printing information in new line
+    printf(" Name : Md Mohibulla Sheikh \n Email : mdmohibullasheikh@gmail.com \n Roll : 2009009 \n Division : Khulna");
+    return 0;
+}
