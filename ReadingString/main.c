6557e4918e698320761d8129b311f48d81de3f5f
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-03-05 13:50:44 +0600

    reading string

---
+++ ReadingString/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // Reading string from terminal and dispalying
+    char text[] = "";
+    printf("Enter your text \n");
+    gets(text);
+    puts(text);
+    return 0;
+}
