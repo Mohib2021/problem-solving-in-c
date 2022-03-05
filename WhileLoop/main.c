40647b7ca38e437be979d0f10dbdd52fe7cae0b5
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-03-05 13:41:12 +0600

    using while loop

---
+++ WhileLoop/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    // printing your name many times using while loop
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("My name is Md Mohibulla Sheikh \n");
+        i++;
+    }
+
+    return 0;
+}
